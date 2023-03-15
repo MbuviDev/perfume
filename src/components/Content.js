ca2d29b5bc508b6292b64d584f6836ae0e147ab0
Meshack <mbuvi.meshack99@gmail.com>
2023-03-15 23:52:54 +0300

    tailwind

--- src/components/Content.js
+++ src/components/Content.js
@@ -4,12 +4,12 @@ function Content () {
 
     return (
         <div>
-            <h3> PERFUME</h3>
-            <h1>Gabrielle Essence Eau De Parfum</h1>
+            <h3 className="font-mono"> PERFUME</h3>
+            <h1 className="text-4xl">Gabrielle Essence Eau De Parfum</h1>
             <p> A floral, solar and voluptuous interpretation composed by olivier polge, perfumer-creator for the house of CHANEL </p>
             <h1>$149.99</h1>
-            <h2>$169.99</h2>
-            <button>Add to Cart</button>
+            <h2 className="overline">$169.99</h2>
+            <button className="bg-green-300">Add to Cart</button>
 
         </div>
     )
