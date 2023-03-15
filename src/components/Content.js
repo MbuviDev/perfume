f64e1f5c889e86fb8120b41449eed8d567922e75
Meshack <mbuvi.meshack99@gmail.com>
2023-03-15 23:49:10 +0300

    content

--- src/components/Content.js
+++ src/components/Content.js
@@ -6,7 +6,9 @@ function Content () {
         <div>
             <h3> PERFUME</h3>
             <h1>Gabrielle Essence Eau De Parfum</h1>
-
+            <p> A floral, solar and voluptuous interpretation composed by olivier polge, perfumer-creator for the house of CHANEL </p>
+            <h1>$149.99</h1>
+            
 
         </div>
     )
